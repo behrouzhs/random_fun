f2c112659760dd443c8998d663a80a133e617a1a
behrouzhs <behrouzhs@gmail.com>
2024-11-27 21:25:02 -0500

    Simplifications and refactoring

--- tree_fractal/tree_fractal.py
+++ tree_fractal/tree_fractal.py
@@ -1,25 +1,24 @@
 import math
-import numpy as np
 import matplotlib.pyplot as plt
 
 
-def _draw_tree(order, x1, y1, angle, angle_increment):
-    if order == 1:
+def _draw_tree(level, x1, y1, angle, angle_increment):
+    if level == 1:
         return
     else:
         angle_rad = angle * math.pi / 180
-        x2 = (x1 + int(np.round(math.sin(angle_rad) * order * 10)))
-        y2 = (y1 + int(np.round(math.cos(angle_rad) * order * 10)))
+        x2 = x1 + math.sin(angle_rad) * level * 10
+        y2 = y1 + math.cos(angle_rad) * level * 10
 
         plt.plot([x1, x2], [y1, y2])
 
-        _draw_tree(order-1, x2, y2, angle + angle_increment, angle_increment)
-        _draw_tree(order-1, x2, y2, angle - angle_increment, angle_increment)
+        _draw_tree(level - 1, x2, y2, angle + angle_increment, angle_increment)
+        _draw_tree(level - 1, x2, y2, angle - angle_increment, angle_increment)
 
 
-def draw_tree(order, angle_increment=30):
-    _draw_tree(order, 300, 300, 0, angle_increment)
+def draw_tree(levels=8, angle_increment=30):
+    _draw_tree(levels, 0, 0, 0, angle_increment)
 
 
-draw_tree(12, 20)
+draw_tree(levels=12, angle_increment=25)
 plt.show()
